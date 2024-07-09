3d5f6c1824a5ea31432cf24ea716a1dcbba9188a
Samin <ksamin4@gmail.com>
2024-07-09 14:37:11 +0530

    Added Dice and main class files.

---
+++ src/main/java/org/swiggyAssignment/Dice.java
@@ -0,0 +1,16 @@
+package org.swiggyAssignment;
+
+import java.util.Random;
+
+public class Dice {
+    private Random random;
+
+    public Dice() {
+        this.random = new Random();
+    }
+
+    public int roll() {
+        return random.nextInt(6) + 1;
+    }
+}
+
